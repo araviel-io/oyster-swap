cf662e8b2c0fc645afb135ba18c130dcaa77862a
Araviel <contact@araviel.io>
2022-01-10 18:24:31 +0100

    rework events

--- src/utils/eventEmitter.ts
+++ src/utils/eventEmitter.ts
@@ -5,15 +5,19 @@ export class AccountUpdateEvent {
   id: string;
   constructor(id: string) {
     this.id = id;
+    console.log("event emitter MarketUpdateEvent")
   }
 }
 
 export class MarketUpdateEvent {
+  
   static type = "MarketUpdate";
   ids: Set<string>;
   constructor(ids: Set<string>) {
     this.ids = ids;
+    console.log("event emitter MarketUpdateEvent")
   }
+  
 }
 
 export class EventEmitter {
@@ -21,21 +25,23 @@ export class EventEmitter {
 
   onMarket(callback: (args: MarketUpdateEvent) => void) {
     this.emitter.on(MarketUpdateEvent.type, callback);
-
+    console.log("event emitter MarketUpdateEvent")
     return () => this.emitter.removeListener(MarketUpdateEvent.type, callback);
   }
 
   onAccount(callback: (args: AccountUpdateEvent) => void) {
     this.emitter.on(AccountUpdateEvent.type, callback);
-
+    console.log("event emitter MarketUpdateEvent")
     return () => this.emitter.removeListener(AccountUpdateEvent.type, callback);
   }
 
   raiseAccountUpdated(id: string) {
+    console.log("event emitter MarketUpdateEvent")
     this.emitter.emit(AccountUpdateEvent.type, new AccountUpdateEvent(id));
   }
 
   raiseMarketUpdated(ids: Set<string>) {
+    console.log("event emitter MarketUpdateEvent")
     this.emitter.emit(MarketUpdateEvent.type, new MarketUpdateEvent(ids));
   }
 }
